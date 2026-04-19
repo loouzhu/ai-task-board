03fdc4f2f1f12f12322e0fa97c1594f8ce35bbe6
肖鑫 <16514132+xiao-xin-111@user.noreply.gitee.com>
2026-04-19 12:58:40 +0800

    用户主页调整布局

---
+++ src/pages/PersonalHomepage/PersonalInfo/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.less";
+
+export default function PersonalInfo() {
+  return <div className={styles.personalInfo}>info</div>;
+}
---
+++ src/pages/PersonalHomepage/WorkBench/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./index.module.less";
+
+export default function WorkBench() {
+  return <div className={styles.workBench}>workbench</div>;
+}
